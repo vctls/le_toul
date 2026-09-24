60d400d9a55d635726532db539d4e419647e2ef3
Victor Toulouse <vctls.github.b1lda@passmail.net>
2026-09-24 18:22:21 +0200

    chore: remove unused SECRET_KEY

--- api/settings.py
+++ api/settings.py
@@ -9,9 +9,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY", "frabbaglabba")
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") != "False"
 
